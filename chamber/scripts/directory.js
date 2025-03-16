71329b8ee5e24d02fded48a292ec2f8f8372246f
Arthur Weale <ajayrossy340@gmail.com>
2025-03-16 20:15:52 +0200

    Added table list

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -16,38 +16,39 @@ hamburgerMenu.addEventListener("click", () => {
     ctaBtn.classList.toggle("active");
 });
 
+const gridBtn = document.querySelector(".grid-btn");
+const listBtn = document.querySelector(".list-btn");
+
 function member(memberCompany){
-        const businessCard = document.createElement("div"); //set attribute class: business-card
-        const businessCardTop = document.createElement("div"); //set attribute class: bizcard-top
-        const header = document.createElement("h2"); //append into bizcard-top
-        const tagLine = document.createElement("p"); //set att to tag
-        const bottomBottomCard = document.createElement("div"); // set att to bottom card bottom
-        const businessLogo = document.createElement("div"); //set att to business-logo ....append into bottom-card-bottom
-        const businessInfo = document.createElement("div"); //set att to business-info....append into bottom-card-bottom
-        const emailPara = document.createElement("p"); //set att to email ....append into business-info
-        const phonePara = document.createElement("p"); //set att to phone ....append into business-info
-        const urlPara = document.createElement("p"); //set att to url ....append into business-info
-        
+    const businessCard = document.createElement("div"); //set attribute class: business-card
+    const businessCardTop = document.createElement("div"); //set attribute class: bizcard-top
+    const header = document.createElement("h2"); //append into bizcard-top
+    const tagLine = document.createElement("p"); //set att to tag
+    const bottomBottomCard = document.createElement("div"); // set att to bottom card bottom
+    const businessLogo = document.createElement("div"); //set att to business-logo ....append into bottom-card-bottom
+    const businessInfo = document.createElement("div"); //set att to business-info....append into bottom-card-bottom
+    const emailPara = document.createElement("p"); //set att to email ....append into business-info
+    const phonePara = document.createElement("p"); //set att to phone ....append into business-info
+    const urlPara = document.createElement("p"); //set att to url ....append into business-info
+    
 
-        businessCard.setAttribute("class", "business-card");
-        businessCardTop.setAttribute("class", "bizcard-top");
-        header.setAttribute("class", "business-card");
-        tagLine.setAttribute("class", "tag");
-        bottomBottomCard.setAttribute("class", "bottom-card-bottom");
-        businessLogo.setAttribute("class", "business-logo");
-        businessInfo.setAttribute("class", "business-info");
-        emailPara.setAttribute("class", "email");
-        phonePara.setAttribute("class", "phone");
-        urlPara.setAttribute("class", "url");
+    businessCard.setAttribute("class", "business-card");
+    businessCardTop.setAttribute("class", "bizcard-top");
+    header.setAttribute("class", "business-card");
+    tagLine.setAttribute("class", "tag");
+    bottomBottomCard.setAttribute("class", "bottom-card-bottom");
+    businessLogo.setAttribute("class", "business-logo");
+    businessInfo.setAttribute("class", "business-info");
+    emailPara.setAttribute("class", "email");
+    phonePara.setAttribute("class", "phone");
+    urlPara.setAttribute("class", "url");
 
-        header.textContent = memberCompany.name;
-        tagLine.textContent = memberCompany.tagline;
-        emailPara.textContent = memberCompany.email;
-        phonePara.textContent = memberCompany.phone;
-        urlPara.textContent = memberCompany.website;
+    header.textContent = memberCompany.name;
+    tagLine.textContent = memberCompany.tagline;
+    emailPara.textContent = memberCompany.email;
+    phonePara.textContent = memberCompany.phone;
+    urlPara.textContent = memberCompany.website;
 
-
-                
     // Build the hierarchy
     // Append header and tagline to the top section container
     businessCardTop.appendChild(header);
@@ -68,9 +69,49 @@ function member(memberCompany){
 
     // Finally, append the business card to the container in your HTML (e.g., a div with the class "bottom-card-container")
     document.querySelector(".bottom-card-container").appendChild(businessCard);
-
 }
 
+
+function populateTable(item){
+    
+    //Toggled List ===================================
+
+    const table = document.createElement("table");
+    table.setAttribute("class", "listview-table");
+    const tbody = document.createElement("tbody");
+    const tableRow = document.createElement("tr");
+    tbody.setAttribute("class", "table-body");
+
+    const businessNameCell = document.createElement("td");
+    const taglineCell = document.createElement("td");
+    const emailCell = document.createElement("td");
+    const phoneCell = document.createElement("td");
+    const websiteCell = document.createElement("td");
+
+    businessNameCell.textContent = item.name;
+    taglineCell.textContent = item.tagline;
+    emailCell.textContent = item.email;
+    phoneCell.textContent = item.phone;
+    websiteCell.textContent = item.website;
+
+    // if (index % 2 == 0) {
+    //     tableRow.style.backgroundColor= "#94D2BD";
+    // } else {
+    //     tableRow.style.backgroundColor="#ffffff";
+    // }
+
+     // Append cells to row
+    tableRow.appendChild(businessNameCell);
+    tableRow.appendChild(taglineCell);
+    tableRow.appendChild(emailCell);
+    tableRow.appendChild(phoneCell);
+    tableRow.appendChild(websiteCell);
+
+    tbody.appendChild(tableRow);
+    table.appendChild(tbody);
+
+    document.querySelector(".bottom-card-container").appendChild(table);
+}
     
 
 
@@ -79,7 +120,8 @@ async function getData() {
     const response = await data.json();
     response  .forEach((company) => {
         console.log(company.name);
-        member(company);
+        //member(company);
+        populateTable(company)
     });
     console.table(response.name);
     
